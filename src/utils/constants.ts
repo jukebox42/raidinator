83dd0474b77d4f3fe4932e9e3c70ad4c0781914d
John <jukebox42@gmail.com>
2022-02-27 16:06:59 -0500

    bump version

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 import { createTheme } from "@mui/material";
 
-export const VERSION = "0.0.3b";
+export const VERSION = "0.0.4b";
 
 // API
 export const DEV_MODE = process.env.NODE_ENV === "development";
